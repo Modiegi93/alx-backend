a5caae1aa6fead54e51ff3cc1e8bad09968d094d
Mathapelo <modiegi.mphahlele93@gmail.com>
2023-08-01 14:17:35 +0200

    fix task 0

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python3
-"""
-Basic Flask app
-"""
+"""Basic Flask app"""
 from flask import Flask, render_template
-
 app = Flask(__name__)
 
+
 @app.route('/')
-def index():
+def greeting():
     """Route for index page"""
-    return render_template('index.html')
+    return render_template('0-index.html')
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port="5000")
